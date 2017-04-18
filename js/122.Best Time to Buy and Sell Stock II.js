60f403444dbc5cf08d32970d7765f56635000f04
zjsxdyw <zjsxdyw@gmail.com>
2017-04-18 21:53:26 +0800

    Create 122.Best Time to Buy and Sell Stock II.js

---
+++ js/122.Best Time to Buy and Sell Stock II.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+//Runtime beats 52.82% of javascript submissions.
+var maxProfit = function(prices) {
+    let profit = 0;
+    for(let i = 1; i < prices.length; i++){
+        if(prices[i] > prices[i-1]){
+            profit += (prices[i] - prices[i-1]);
+        }
+    }
+    return profit;
+};
